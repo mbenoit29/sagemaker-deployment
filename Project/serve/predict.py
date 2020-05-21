3891022f7e861137cc7398255883db11086fe300
mbenoit29 <mabe@hellofresh.com>
2020-05-21 17:51:00 +0200

    Complete project

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -72,6 +72,8 @@ def predict_fn(input_data, model):
 
     data_X = None
     data_len = None
+    words_input = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, words_input)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -88,5 +90,7 @@ def predict_fn(input_data, model):
     #       be a numpy array which contains a single integer which is either 1 or 0
 
     result = None
-
+    prediction = model(data).detach()
+    out = prediction.numpy()
+    result = int(np.round(out))
     return result
